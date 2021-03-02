a2760b43d4754178f4ae5c0a879d98811748e0dd
kaisei <kainana0015@gmail.com>
2021-03-02 16:36:30 +0900

    Initial Commit

---
+++ JTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JTalk
+//
+//  Created by 岩田海靖 on 2021/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
